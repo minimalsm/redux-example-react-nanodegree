35f2aa1839bfb73858ead6cccbb3342ea651c93d
Joshua <30259508@cityofglacol.ac.uk>
2020-08-04 08:53:55 +0100

    Moved App from index.html to App component

--- udacity-goals-todos/src/components/App.js
+++ udacity-goals-todos/src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import ConnectedGoals from './Goals'
+import ConnectedTodos from './Todos'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
-const App = () => {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount () {
+      const { dispatch } = this.props
+      dispatch(handleInitialData())
+    }
+
+  render() {
+    if (this.props.loading) {
+      return <h3>Loading....</h3>
+    }
+
+    return (
+      <div>   
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
